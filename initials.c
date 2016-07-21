0a95e9dab488aeaa192673029ba7667808fb8732
Albert Zhang <zhangtheboomerang@gmail.com>
2016-07-20 20:57:19 -0400

    Albert finished assignment 1

---
+++ initials.c
@@ -0,0 +1,15 @@
+/*Albert Zhang
+ *Prints AZ using pound signs */
+#include <stdio.h>
+int main ()
+{
+	printf("	#          ###### \n");
+	printf("       #  #            #  \n");
+	printf("      #    #          #   \n");
+	printf("      #######        #    \n");
+	printf("      #     #       #     \n");
+	printf("      #     #      ###### \n");
+
+
+	return 0;
+}
