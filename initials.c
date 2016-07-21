97dfbc3bfda313741c99188121c5c39f0c4e3a17
Rebecca Hale <rkh2128@coumbia.edu>
2016-07-21 14:04:09 -0400

    added assignment 1 files

---
+++ initials.c
@@ -0,0 +1,13 @@
+/*Rebecca Hale, Part 2 Assignment 1*/
+
+#include <stdio.h>
+
+  int main(){
+
+	  printf("######    ##  ##\n");
+	  printf("##  ##    ##  ##\n");
+	  printf("####      ######\n");
+	  printf("##  ##    ##  ##\n");
+	  printf("##  ##    ##  ##\n");
+	  return 0;
+  }
