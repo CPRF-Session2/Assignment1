7cb1ff129ee4d1250e564d19cd6da819d58f689a
Armstrongpark <ap3545@columbia.edu>
2016-07-20 22:17:56 -0400

    Armstrong Park initials and theory on errors

---
+++ initials.c
@@ -0,0 +1,13 @@
+#include <stdio.h>    /*Armstrong Park - printing initials program */
+
+int main()
+
+{
+	printf("       	           #       #####\n ");
+	printf("		  # #      #    #\n");
+	printf("		 #   #     # ### \n");
+	printf("	        #######    # \n");
+	printf("	       #       #   #\n");
+	printf("              #         #  #\n");
+	return 0;
+}
