de1e529daaaa531fa3d0aea3d24d9bb5138690f8
Jinoo Hong <ejh2190@columbia.edu>
2016-07-20 20:18:09 -0400

    I finished the homework!

---
+++ initials.c
@@ -0,0 +1,16 @@
+/*Jinoo Hong*/
+/*Prints my initials with pound signs*/
+
+#include <stdio.h>
+
+int main()
+{
+	printf("\n#########   ###     ###\n");
+	printf("     #      ###     ###\n");
+	printf("     #      ###     ###\n");
+     	printf("     #      ###########\n");
+	printf("     #      ###     ###\n");
+	printf(" #   #      ###     ###\n");
+	printf(" #####      ###     ###\n");
+	return 0;
+}
